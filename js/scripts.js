4dc642d78902867ac82e968565995fd2d1252abc
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-02-25 22:25:40 -0300

    Montar Pedido - seleciona 1 opção por categoria

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,31 @@
+let modeloEscolhido = null, golaEscolhida = null, tecidoEscolhido = null;
+// let nome = prompt("Insira seu nome de usuário:");
+// lembrar de desconmentar o prompt!!!!!!!!!!!!!!!
 
-let nome = prompt("Insira seu nome de usuário:");
+function selecionarModelo(opcaoModelo) {
+    if(modeloEscolhido !== null) {
+        modeloEscolhido.classList.remove("selecionado");
+    }
+   
+    opcaoModelo.classList.add("selecionado");
+    modeloEscolhido = opcaoModelo;
+}
+
+function selecionarGola(opcaoGola) {
+    if(golaEscolhida !== null) {
+        golaEscolhida.classList.remove("selecionado");
+    }
+   
+    opcaoGola.classList.add("selecionado");
+    golaEscolhida = opcaoGola;
+}
+
+function selecionarTecido(opcaoTecido) {
+    if(tecidoEscolhido !== null) {
+        tecidoEscolhido.classList.remove("selecionado");
+    }
+   
+    opcaoTecido.classList.add("selecionado");
+    tecidoEscolhido = opcaoTecido;
+}
+
